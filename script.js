a004453621eccbcdf3e3f3bd9195a6122ad78e04
VinMainframe <150822224+VinMainframe@users.noreply.github.com>
2023-11-14 22:36:16 +0600

    Update script.js
    update 2nd time

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ function submitForm() {
     var email = document.getElementById("email").value;
     var age = document.getElementById("age").value;
 
-    var displayDataDiv = document.getElementById("displayData");
-    displayDataDiv.innerHTML = "<p><strong>Name:</strong> " + name + "</p>" +
-                               "<p><strong>Email:</strong> " + email + "</p>" +
-                               "<p><strong>Age:</strong> " + age + "</p>";
+    // Display data on the console (you can modify this to do other things)
+    console.log("Name: " + name);
+    console.log("Email: " + email);
+    console.log("Age: " + age);
+
+    // Optionally, you can redirect to another page
+    // window.location.href = "display.html";
 }
